1cf308a8b2d9a41018fbfc71b36244b4c33d8a6a
Ariel Reuven Kogan <ar.kogan@gmail.com>
2020-09-17 18:27:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Add your doToElementsInArray() function here:
 var fruit = ['apple', 'banana', 'cherry'];
-const doToElementsInArray = fruit.forEach((fruit, index, array) => {
-  console.log(`Mmmm, ${fruit}!`);
-});
+doToElementsInArray() {
+  fruit.forEach((fruit, index, array) => {
+    return(`Mmmm, ${fruit}!`);
+  });  
+}
 
 // Add your changeCompletely() function here:
