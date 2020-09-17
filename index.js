eb6cdecf28e8489f181ef80c8922441f70d3ecb2
Ariel Reuven Kogan <ar.kogan@gmail.com>
2020-09-17 18:09:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Add your doToElementsInArray() function here:
+const array = ["apple", "banana", "cherry"];
 doElementsInArray = (array, callback) => {
-  
+
 }
 
 // Add your changeCompletely() function here:
